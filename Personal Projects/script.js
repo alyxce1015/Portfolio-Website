b0a070b88ce4b43a03efe882eced4c5df4ecc347
alyxce1015 <acui@csu.fullerton.edu>
2025-04-27 14:08:57 -0700

    implemented carousel feature. REMOVED old project display and switched to carousel

--- Personal Projects/script.js
+++ Personal Projects/script.js
@@ -2,8 +2,7 @@
 
 // if you wish to change the words just adjust these words
 const texts = [
-    "Student",
-    "CSUF"
+    "Alyx Cui Edio"
 ]
 
 let speed = 50;
@@ -36,3 +35,35 @@ function eraseText() {
 }
 
 window.onload = typeWriter
+
+
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
